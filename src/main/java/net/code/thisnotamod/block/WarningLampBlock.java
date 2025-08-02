9c47a389d176c8762af65c5c452f5f2746cfd0d4
Kit_1112 <boombeat.team@gmail.com>
2025-08-02 19:22:10 +0300

    1.8.7.2a
    ✅ По аварийной лампе больше нельзя кликнуть ПКМ

--- src/main/java/net/code/thisnotamod/block/WarningLampBlock.java
+++ src/main/java/net/code/thisnotamod/block/WarningLampBlock.java
@@ -5,7 +5,6 @@ import org.checkerframework.checker.units.qual.s;
 
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
@@ -31,13 +30,10 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.MenuProvider;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.Containers;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
-import net.code.thisnotamod.procedures.WarningLampPriShchielchkiePKMPoBlokuProcedure;
 import net.code.thisnotamod.procedures.WarnLampPlacedProcedure;
 import net.code.thisnotamod.procedures.WarnLampDestrProcedure;
 import net.code.thisnotamod.init.ThisnotamodModBlockEntities;
@@ -141,21 +137,6 @@ public class WarningLampBlock extends BaseEntityBlock implements EntityBlock {
 	public void setPlacedBy(Level world, BlockPos pos, BlockState blockstate, LivingEntity entity, ItemStack itemstack) {
 		super.setPlacedBy(world, pos, blockstate, entity, itemstack);
 		WarnLampPlacedProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
-	}
-
-	@Override
-	public InteractionResult use(BlockState blockstate, Level world, BlockPos pos, Player entity, InteractionHand hand, BlockHitResult hit) {
-		super.use(blockstate, world, pos, entity, hand, hit);
-		int x = pos.getX();
-		int y = pos.getY();
-		int z = pos.getZ();
-		double hitX = hit.getLocation().x;
-		double hitY = hit.getLocation().y;
-		double hitZ = hit.getLocation().z;
-		Direction direction = hit.getDirection();
-
-		WarningLampPriShchielchkiePKMPoBlokuProcedure.execute(world, x, y, z, blockstate);
-		return InteractionResult.SUCCESS;
 	}
 
 	@Override
